b9e03b2fbbbe031504de237872ace3e0245f7cab
Venilton FalvoJr <falvojr@gmail.com>
2017-09-18 12:17:51 -0300

    TaskMaker Certification Exam (initial project)

---
+++ app/src/main/java/com/google/developer/taskmaker/MainActivity.java
@@ -0,0 +1,73 @@
+package com.google.developer.taskmaker;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.google.developer.taskmaker.data.TaskAdapter;
+
+public class MainActivity extends AppCompatActivity implements
+        TaskAdapter.OnItemClickListener,
+        View.OnClickListener {
+
+    private TaskAdapter mAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        mAdapter = new TaskAdapter(null);
+        mAdapter.setOnItemClickListener(this);
+
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setAdapter(mAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            Intent intent = new Intent(this, SettingsActivity.class);
+            startActivity(intent);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    /* Click events in Floating Action Button */
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent(this, AddTaskActivity.class);
+        startActivity(intent);
+    }
+
+    /* Click events in RecyclerView items */
+    @Override
+    public void onItemClick(View v, int position) {
+        //TODO: Handle list item click event
+    }
+
+    /* Click events on RecyclerView item checkboxes */
+    @Override
+    public void onItemToggled(boolean active, int position) {
+        //TODO: Handle task item checkbox event
+    }
+}
---
+++ app/src/main/java/com/google/developer/taskmaker/data/TaskAdapter.java
@@ -0,0 +1,127 @@
+package com.google.developer.taskmaker.data;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.support.v7.widget.RecyclerView;
+import android.text.format.DateUtils;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.developer.taskmaker.R;
+import com.google.developer.taskmaker.views.TaskTitleView;
+
+public class TaskAdapter extends RecyclerView.Adapter<TaskAdapter.TaskHolder> {
+
+    /* Callback for list item click events */
+    public interface OnItemClickListener {
+        void onItemClick(View v, int position);
+
+        void onItemToggled(boolean active, int position);
+    }
+
+    /* ViewHolder for each task item */
+    public class TaskHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        public TaskTitleView nameView;
+        public TextView dateView;
+        public ImageView priorityView;
+        public CheckBox checkBox;
+
+        public TaskHolder(View itemView) {
+            super(itemView);
+
+            nameView = (TaskTitleView) itemView.findViewById(R.id.text_description);
+            dateView = (TextView) itemView.findViewById(R.id.text_date);
+            priorityView = (ImageView) itemView.findViewById(R.id.priority);
+            checkBox = (CheckBox) itemView.findViewById(R.id.checkbox);
+
+            itemView.setOnClickListener(this);
+            checkBox.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+            if (v == checkBox) {
+                completionToggled(this);
+            } else {
+                postItemClick(this);
+            }
+        }
+    }
+
+    private Cursor mCursor;
+    private OnItemClickListener mOnItemClickListener;
+    private Context mContext;
+
+    public TaskAdapter(Cursor cursor) {
+        mCursor = cursor;
+    }
+
+    public void setOnItemClickListener(OnItemClickListener listener) {
+        mOnItemClickListener = listener;
+    }
+
+    private void completionToggled(TaskHolder holder) {
+        if (mOnItemClickListener != null) {
+            mOnItemClickListener.onItemToggled(holder.checkBox.isChecked(), holder.getAdapterPosition());
+        }
+    }
+
+    private void postItemClick(TaskHolder holder) {
+        if (mOnItemClickListener != null) {
+            mOnItemClickListener.onItemClick(holder.itemView, holder.getAdapterPosition());
+        }
+    }
+
+    @Override
+    public TaskHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        mContext = parent.getContext();
+        View itemView = LayoutInflater.from(mContext)
+                .inflate(R.layout.list_item_task, parent, false);
+
+        return new TaskHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(TaskHolder holder, int position) {
+
+        //TODO: Bind the task data to the views
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return (mCursor != null) ? mCursor.getCount() : 0;
+    }
+
+    /**
+     * Retrieve a {@link Task} for the data at the given position.
+     *
+     * @param position Adapter item position.
+     *
+     * @return A new {@link Task} filled with the position's attributes.
+     */
+    public Task getItem(int position) {
+        if (!mCursor.moveToPosition(position)) {
+            throw new IllegalStateException("Invalid item position requested");
+        }
+
+        return new Task(mCursor);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return getItem(position).id;
+    }
+
+    public void swapCursor(Cursor cursor) {
+        if (mCursor != null) {
+            mCursor.close();
+        }
+        mCursor = cursor;
+        notifyDataSetChanged();
+    }
+}
